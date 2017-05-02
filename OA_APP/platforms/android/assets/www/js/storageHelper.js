6dd590e48c2103fe3586f9c5fea6b9db333f8d05
cicixiaoyang <15608066283@163.com>
2017-05-02 18:10:15 +0800

    fff

---
+++ OA_APP/platforms/android/assets/www/js/storageHelper.js
@@ -0,0 +1,50 @@
+﻿//javascript对storage进行增删改查的操作
+var storageHelper = function () {
+}
+
+storageHelper.addStorage = function (key, value, isSession) {
+    switch (typeof value) {
+        case "object":
+            value = JSON.stringify(value);
+            break;
+    }
+
+    storageHelper.delStorage(key);
+    if (isSession) {
+        sessionStorage.setItem(key, value);
+    }
+    else {
+        localStorage.setItem(key, value);
+    }
+}
+
+storageHelper.getStorage = function (key) {
+    var result = "";
+    var localValue = localStorage.getItem(key);
+    switch (localValue) {
+        case null:
+            result = sessionStorage.getItem(key);
+            break;
+        default:
+            result = localValue;
+            break;
+    }
+
+    try {
+        return eval("(" + result + ")");
+    }
+    catch (e) {
+        return result;
+    }
+}
+
+storageHelper.delStorage = function (key) {
+    switch (localStorage.getItem(key)) {
+        case null:
+            return sessionStorage.removeItem(key);
+            break;
+        default:
+            return localStorage.removeItem(key);
+            break;
+    }
+}
--- OA_WEBApp/src/pages/mail/mail.ts
+++ OA_WEBApp/src/pages/mail/mail.ts
@@ -14,7 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Mail {
   box: string = "inbox";
-
+  checkBtn: object = { read: false, unread: true, all: false };
+  isDraft: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }

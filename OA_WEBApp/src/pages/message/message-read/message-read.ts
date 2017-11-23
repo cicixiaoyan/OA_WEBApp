9387a61b6966672e808f45cb3003782723fe8ccc
AFOCL-703201547\Administrator <15608066283@163.com>
2017-11-23 23:42:34 +0800

    message-read

--- OA_WEBApp/src/pages/message/message-read/message-read.ts
+++ OA_WEBApp/src/pages/message/message-read/message-read.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, ModalController, NavParams } from 'ionic-angular';
+import { Content } from 'ionic-angular';
+import { NativeService } from "../../../providers/NativeService";
+
+import { Transfer } from '@ionic-native/transfer'; // , FileUploadOptions, TransferObject
+import { File } from '@ionic-native/file';
+
+import { MessageService } from '../messageService';
 
 /**
  * Generated class for the MessageReadPage page.
@@ -14,12 +21,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'message-read.html',
 })
 export class MessageReadPage {
+    @ViewChild(Content) content: Content;
+    messageContent: string = ""; // 消息内容
+    messageDetail: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private nativeService: NativeService,
+                private modalCtrl: ModalController,
+                private transfer: Transfer,
+                private messageService: MessageService,
+                private file: File) {
+        this.initializeItems();
+    }
+    initializeItems() {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MessageReadPage');
-  }
+        this.messageService.read(this.navParams.get('id')).subscribe((resJson) => {
+            if (resJson.Result){
+                this.messageDetail = resJson.Data;
+            }
+        });
+
+        // this.messageContent = this.messageDetail.ggnr;
+    }
 
 }
